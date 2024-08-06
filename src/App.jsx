b6171f732e98eab8c50cc575c431e5fecbc2260f
Nodsader <nodsader@gmail.com>
2024-08-06 16:52:35 +0200

    Modifications légères dans la structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import { GsapTo,
-  GsapFrom,
-  GsapFromTo,
-GsapTimeline,
-GsapStagger,
-GsapScrollTrigger,
-GsapText,
-Home} from "./pages"
-
 const App = () => {
-  return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Hello World!</h1>
+		</div>
+	)
+}
 
 export default App;
