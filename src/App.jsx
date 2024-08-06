cc7da256bb3e099e64c52401ee383c697ff39b68
Nodsader <nodsader@gmail.com>
2024-08-06 21:16:28 +0200

    NavBar good :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
+
 const App = () => {
 	return (
 		<main className={"bg-black"}>
-			Navbar
-			Hero
-			Highlights
+			<NavBar/>
+			<Hero/>
+			<Highlights/>
 		</main>
 	)
 }
