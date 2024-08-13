65505b592748f12ed3891cf7216c4fb273e613ec
Nodsader <nodsader@gmail.com>
2024-08-13 23:48:32 +0200

    MAJ

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <section className={"w-full nav-height bg-black relative"}>
             <div className={"h-5/6 w-full flex-center flex-col"}>
                 <p id={"hero"} className={"hero-title"}>Madame Pascale, un iPhone 15 Pro ?</p>
-                <div className={"md:w-10/12 w-9/12"}>
+                <div className={"md:w-10/12 w-8/12"}>
                     <video autoPlay={true} muted={true} key={videoSrc} playsInline={true} className={"pointer-events-none"}>
                         <source src={videoSrc} type={"video/mp4"}/>
                     </video>
