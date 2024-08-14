90d6522c4bdaa2941da5f7f0efabfce9d5ba2384
Nodsader <nodsader@gmail.com>
2024-08-14 14:57:56 +0200

    Correctif responsive Safari

--- src/components/videoCarousel.jsx
+++ src/components/videoCarousel.jsx
@@ -179,7 +179,7 @@ const VideoCarousel = () => {
                 ))}
             </div>
 
-            <div className={"relative flex-center mt-10"}>
+            <div className={"relative flex-center mt-10 overflow-hidden"}>
                 <div className={"flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full"}>
                     {videoRef.current.map((_, i) => (
                         <span key={i}
