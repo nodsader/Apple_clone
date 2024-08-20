0eb96d16b13553a27f08e05207cabc19de2b9d20
Nodsader <nodsader@gmail.com>
2024-08-20 10:24:16 +0200

    Modifications des lignes changées pour la dédicasse

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
     return (
         <section className={"w-full nav-height bg-black relative"}>
             <div className={"h-5/6 w-full flex-center flex-col"}>
-                <p id={"hero"} className={"hero-title"}>Madame Pascale, un iPhone 15 Pro ?</p>
+                <p id={"hero"} className={"hero-title"}> iPhone 15 Pro</p>
                 <div className={"md:w-10/12 w-8/12"}>
                     <video autoPlay={true} muted={true} key={videoSrc} playsInline={true} className={"pointer-events-none"}>
                         <source src={videoSrc} type={"video/mp4"}/>
